8fd73f09b905b40a69b77c890fc92dc109a39fb4
ColdShaker <michaelball357@gmail.com>
2024-03-13 22:54:07 -0700

    Not working idk why

---
+++ install_depencies.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+npm i sass
+npm i react-router-dom
+npm i react-loaders
+npm i react-leaflet
+npm i loaders.css
+npm i gsap trial
+npm i animate.css
+npm i @fortawesome/react-fontawesome
+npm i @fortawesome/free-solid-svg-icons
+npm i @fortawesome/free-brands-svg-icons
+npm i @emailjs/browser
---
+++ src/components/Layout/index.js
@@ -0,0 +1,9 @@
+import Sidebar from '../Sidebar'
+import './index.scss'
+
+const Layout = () => {
+    console.log("The code gets here")
+    return <>test</>;
+}
+
+export default Layout
